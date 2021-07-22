1bea3e02c2466575a43b1e66d4542a4bda7f44ce
hythang2020 <hythang2020@gmail.com>
2021-07-22 21:34:27 +0700

    up file

--- Day8/hy.py
+++ Day8/hy.py
@@ -1,14 +1,25 @@
-customers = []
+# customers = []
 
-def add_customer(data):
-    id, name = data.split(',')
-    customers.append({
-        "id" : id,
-        "name" : name,
-    })
+# def add_customer(data):
+#     id, name = data.split(',')
+#     customers.append({
+#         "id" : id,
+#         "name" : name,
+#     })
 
-f = open('data.txt', 'r')
-for i in f.readlines():
-    add_customer(i)
+# f = open('data.txt', 'r')
+# for i in f.readlines():
+#     add_customer(i)
 
-print(customers)
+# print(customers)
+
+def giaithua(n):
+    giai_thua = 1
+    if (n ==0 or n ==1 ):
+        return giai_thua
+    else:
+        for i in range(2, n +1):
+            giai_thua = giai_thua * i
+            return giai_thua
+
+print (giaithua(3))
