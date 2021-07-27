b410f536c033d2073059563287981fbf16560680
RayNguyen842857 <cloudstrife5000@gmail.com>
2021-07-27 22:28:27 +0900

    update

--- Day9/NMQuang.py
+++ Day9/NMQuang.py
@@ -4,21 +4,28 @@ from database import Dabatase
 class Product():
     def __init__(self, id, name, price):
 
-        if type(price) != 'float' or type(id) != 'int':
-            print('Wrong format!')
+        # if type(price) != 'float' or type(id) != 'int':
+        #     print('Wrong format!')
 
-        self.id = id
+        self.__id = id
         self.name = str(name)
         self.price = price
+
+    def get_id(self):
+        return self.__id
+
+    def set_id(self):
+        self.__id = id
+
+#     def set_name(self, name):
+#         self.name = name
+# 
+#     def set_price(self, price):
+#         self.price = price
 
 
 def main():
     db = Dabatase()
-    television = Product(10, "32inch", 299.99)
-    print(television.id)
-    print(television.name)
-    print(television.price)
-
     list_products_object = []
     for product in db.list_products:
         obj = Product(product['id'], product['name'], product['price'])
