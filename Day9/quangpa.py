b56911d886cd3c4ee9eead2b174c476f48163b41
QuangPa <phamanhquangit@gmail.com>
2021-07-27 21:24:40 +0700

    Show san pham

--- Day9/quangpa.py
+++ Day9/quangpa.py
@@ -8,20 +8,30 @@ class Product:
         return self.__id
     def set_id(self, id):
         self.__id = id
+class Shop:
+    def __init__(self, name, list_products):
+        self.name = name
+        self.list_products = list_products
+    def show(self):
+        for product in self.list_products:
+            print("Amount:{0} Name:{2} Amount_sold:{1}".format(product['amount'], product['amount_sold'], product['product'].name))
+
+
 
 if __name__ == "__main__":
-    # db = Dabatase()
+    db = Dabatase()
     
-    # list_product_objects = []
+    list_product_objects = []
 
-    # for product in db.list_products:
-    #     obj = Product(product['id'], product['name'], product['price'])
-    #     list_product_objects.append(obj)
-
+    for product in db.list_products:
+        obj = Product(product['id'], product['name'], product['price'])
+        list_product_objects.append({
+            "product": obj,
+            "amount": 10,
+            "amount_sold":1
+        })
+    # print(list_products_object)
     # for product in list_product_objects:
     #     print(product.name)
-    quan = Product(1,"quan", 100)
-    quan.id = 2
-    quan.set_id(2)
-    print(quan.get_id())
-
+    shop1 = Shop("Shop 1", list_product_objects)
+    shop1.show()    
