ab4a08c5c174d102638755d3d9bc51fbeb4ef0d5
QuangPa <phamanhquangit@gmail.com>
2021-07-27 19:48:06 +0700

    Your message here

--- Day9/quangpa.py
+++ Day9/quangpa.py
@@ -1,11 +1,15 @@
 class Product:
     def __init__(self, id, name, price):
+        print(self)
+        # if type(price) != float or type(name) != str:
+        #     print("Nhap lai")
+        #     return
         self.id = id
         self.name = name
         self.price = price
-        if type(price) != float:
-            print("Nhap lai")
-        elif type(name) != str:
-            print("Nhập lại")
-        else:
-            continue
+
+
+
+if __name__ == "__main__":
+    p = Product(1,"Quan", 100)
+    print(p.name)
