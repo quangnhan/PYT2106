5c429789e95f876daab47781e863749c7c18b618
RayNguyen842857 <cloudstrife5000@gmail.com>
2021-07-27 21:40:09 +0900

    update

---
+++ Day9/NMQuang.py
@@ -0,0 +1,16 @@
+class Product():
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
+
+
+def main():
+    television = Product(10, "32inch", "299.99")
+    print(television.id)
+    print(television.name)
+    print(television.price)
+
+
+if __name__ == "__main__":
+    main()
