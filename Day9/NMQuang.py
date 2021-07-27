4130fc76d3f5b0d7a7b5109da6c50de717f406ec
RayNguyen842857 <cloudstrife5000@gmail.com>
2021-07-27 21:42:45 +0900

    update

--- Day9/NMQuang.py
+++ Day9/NMQuang.py
@@ -1,12 +1,16 @@
 class Product():
     def __init__(self, id, name, price):
+
+        if type(price) != 'float':
+            print('Wrong format!')
+
         self.id = id
         self.name = name
         self.price = price
 
 
 def main():
-    television = Product(10, "32inch", "299.99")
+    television = Product(10, "32inch", 299.99)
     print(television.id)
     print(television.name)
     print(television.price)
