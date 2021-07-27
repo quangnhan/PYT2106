f4e33d626e2f463e6f77d63506023195f5027d6c
RayNguyen842857 <cloudstrife5000@gmail.com>
2021-07-27 22:08:46 +0900

    update

--- Day9/NMQuang.py
+++ Day9/NMQuang.py
@@ -1,3 +1,6 @@
+from database import Dabatase
+
+
 class Product():
     def __init__(self, id, name, price):
 
@@ -10,10 +13,19 @@ class Product():
 
 
 def main():
+    db = Dabatase()
     television = Product(10, "32inch", 299.99)
     print(television.id)
     print(television.name)
     print(television.price)
+
+    list_products_object = []
+    for product in db.list_products:
+        obj = Product(product['id'], product['name'], product['price'])
+        list_products_object.append(obj)
+
+    for product in list_products_object:
+        print(product.name)
 
 
 if __name__ == "__main__":
