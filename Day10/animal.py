5098b79a83f64c08c8e25884eb1307d6c9f47e09
quangnhan145 <nhan@leadplus.net>
2021-07-29 18:50:26 +0700

    Add new function to animal

--- Day10/animal.py
+++ Day10/animal.py
@@ -10,6 +10,15 @@ class Animal:
     def show(self):
         print(f"This is {self.__name} with weight={self.__weight} and height={self.__height}")
 
+    def shout(self):
+        print("rrrrrrrrrrrr")
+
+    def combo(self):
+        self.show()
+        self.shout()
+
 if __name__ == "__main__":
     lion = Animal("Lion", 100, 50)
     lion.show()
+    lion.shout()
+    lion.combo()
