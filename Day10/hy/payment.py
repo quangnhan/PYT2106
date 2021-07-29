b6c5ed9800341622070eda2e6a2415de7ff9f85f
hythang2020 <hythang2020@gmail.com>
2021-07-29 20:29:44 +0700

    up

---
+++ Day10/hy/payment.py
@@ -0,0 +1,18 @@
+from momo import Momo as M
+from vietcombank import Vietcombank as V
+class Payment:
+    def __init__(self,pay):
+        self.pay = pay
+
+    def charge(self,money):
+        self.pay.chage(money)
+
+
+if __name__ == "__main__":
+    momo = M("0919147526")
+    pay = Payment(momo)
+    pay.charge("500000")
+
+    vietcom = V("0909489521")
+    pay = Payment(vietcom)
+    pay.charge("350000")
---
+++ Day10/hy/vietcombank.py
@@ -0,0 +1,7 @@
+class Vietcombank:
+    def __init__(self, phone):
+        self.phone = phone
+    
+    def chage(self,money):
+        self.money = money
+        print(f"Vietcombank : {self.money}")
