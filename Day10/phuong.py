ace9e9e304012f6aadb76065654dc9fd46cc51c6
Calcierzz <phuongnv510@gmail.com>
2021-07-29 20:13:44 +0700

    update

--- Day10/phuong.py
+++ Day10/phuong.py
@@ -1,8 +1,10 @@
 from animal import Animal
 
 class Peguin(Animal):
-    def __init__(self, name, weight, height):
+    def __init__(self, name, weight, height, leg, color):
         super().__init__(name, weight, height)
+        self.leg = leg
+        self.color = color
 
     def show(self):
         print(f"Agent: {self._name}")
@@ -10,6 +12,18 @@ class Peguin(Animal):
     def set_name(self, name):
         self._name = name
 
+    def show(self):
+        print(f"Agent: {self._name}")
+
+    def get_leg(self):
+        return self.leg
+
+    def get_color(self):
+        return self.color
+
+
 if __name__ == "__main__":
-    peguin = Peguin("Loco", 50, 40)
-    peguin.show()
+    p1 = Peguin("Loco", 50, 40, 2, "pink")
+    #p1.show()
+    p1(Peguin.get_leg())
+    p1(Peguin.get_colour())
