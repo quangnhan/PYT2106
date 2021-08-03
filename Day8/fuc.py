ab4d687f6d73a8117506a921cecee20905b5154f
iaced102 <lightvhp@gmail.com>
2021-08-04 01:12:32 +0700

    update

--- Day8/fuc.py
+++ Day8/fuc.py
@@ -1,3 +1,5 @@
+from server import get_server_data
+
 custommer = []
 
 def Add_Customer(data):
@@ -7,7 +9,24 @@ def Add_Customer(data):
         'name':name
     }
 
+def getdata(data):
+    intern = data
+    for item in intern:
+        id, name = item.split(',')
+        intern[intern.index(item)] = {'id':id, 'name': name} 
+    return intern
+
+'''
 for line in open('Day8/data.txt','r').read().split('\n'):
     print(line)
     custommer.append(Add_Customer(line))
+
 print(custommer)
+'''
+
+data = get_server_data()
+
+for line in getdata(data):
+    custommer.append(line)
+print(custommer)
+
