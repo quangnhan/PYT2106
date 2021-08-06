c213abf38364c7a25750f7e40546592b48221557
Calciferxx <81376984+Calciferxx@users.noreply.github.com>
2021-08-06 15:18:41 +0700

    update

--- Day10/phuong/bai2/Shop.py
+++ Day10/phuong/bai2/Shop.py
@@ -1,31 +1,42 @@
-# class Shop:
-#     def __init__(self, name, list_products):
-#         self.name = name
-#         self.list_products = list_products
+# import math
 
-#     def get_name(self):
-#         return self.name
+# a = int(input())
+# b = int(input())
+# c = int(input())
 
-#     def get_info(self):
-#         print (self.name, self.list_products):
-#         for item in self.list_products:
-#             print(f'---------------\nAmount')
+# def alpha(a, b, c):
+#     alpha = float(math.acos((a**2 + b**2- c**2)/2*a*b))
+#     return alpha
 
-import math
+# if a+b >= c or b+c >= a or a+c >= b:
+#     if a == b and b == c and c == a:
+#         # alpha == 60
+#         print("Equilateral triangle")
+#     if a == b or a == c or b == c:
+#         # alpha != 60
+#         print("Isosceles triangle")
+#     if a != b and a != c and c != b:
+#         print("Scalene triangle")
 
-a = int(input())
-b = int(input())
-c = int(input())
+from database import Dabatase
 
-def alpha(a, b, c):
-    alpha = float(math.acos((a**2 + b**2- c**2)/2*a*b))
-    return alpha
 
-if a+b >= c or b+c >= a or a+c >= b:
-    if a == b and b == c and c == a:
-        print("Equilateral triangle")
-    if a == b or a == c or b == c:
-        alpha != 60
-        print("Isosceles triangle")
-    if a != b and a != c and c != b:
-        print("Scalene triangle")
+class Shop:
+    def __init__(self, name, list_products):
+        self.name = name
+        self.list_products = list_products
+
+    def get_name(self):
+        return self.name
+
+    def get_info(self):
+        print (self.name, self.list_products):
+        for item in self.list_products:
+            print(f'---------------\nAmount')
+            print(f"Amount: {item['amount']}, Product: {item['product'].get_name()},Sold: {item['sold_amount']}")
+
+if __name__ == "__main__":
+    db = Dabatase
+    list_product_object = []
+
+    
