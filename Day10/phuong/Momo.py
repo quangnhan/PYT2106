f2839b514a1e0c5852631fe46cd95f85fa6c2239
QuangPa <phamanhquangit@gmail.com>
2021-08-09 23:53:24 +0700

    Bai 2

--- Day10/phuong/Momo.py
+++
@@ -1,6 +0,0 @@
-class Momo():
-    def __init__(self, phone):
-        self.phone = phone
-
-    def charge(self, money):
-        print(f'Momo charge {money}')
--- Day10/phuong/Vietcombank.py
+++
@@ -1,6 +0,0 @@
-class Vietcombank():
-    def __init__(self, phone):
-        self.phone = phone
-
-    def charge(self, money):
-        print(f'Vietcombank charge {money}')
