2e7126ae8a3a5e61ccbad24a2bfc587910ac7f68
Hy B <b-tin@tuk>
2021-08-10 19:45:05 +0700

    minhhy - bai 11 - filereader

---
+++ Day11/minhhy/CSV.py
@@ -0,0 +1,16 @@
+import os
+import csv
+
+class CSV:
+    def __init__(self, path):
+        f = open(path)
+        self.csv = csv.reader(f, delimiter=',')
+
+    def show(self):
+        for row in self.csv:
+            print(row)
+
+if __name__ == "__main__":
+    path = f'{os.getcwd()}/data/file.csv'
+    csv_file = CSV(path)
+    csv_file.show()
