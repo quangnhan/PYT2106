190d09eae28a8a58471ab5d71d57b5ea8bfd7c98
QuangPa <phamanhquangit@gmail.com>
2021-08-17 19:40:24 +0700

    Post, get by id

--- Day12/quangpa/post.py
+++ Day12/quangpa/post.py
@@ -1,7 +1,11 @@
 import requests
 
+data = {
+    'name': 'ABC',
+    'age':20,
+}
 url = 'https://611ba73022020a00175a4615.mockapi.io/User'
 
-response = requests.get(url)
+response = requests.post(url,data = data)
 data = response.json()
 print(data)
