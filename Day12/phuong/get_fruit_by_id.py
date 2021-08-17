feb5dcc6d4718d4e915c8d90034d9ef35a42ca8a
Calcierzz <phuongnv510@gmail.com>
2021-08-17 19:42:59 +0700

    updt

--- Day12/phuong/get_fruit_by_id.py
+++ Day12/phuong/get_fruit_by_id.py
@@ -1,6 +1,7 @@
 import requests
 
-url = "https://611ba8ad22020a00175a4622.mockapi.io/fruit_name"
+id = 1
+url = f"https://611ba8ad22020a00175a4622.mockapi.io/fruit_name/{id}"
 response = requests.get(url)
 data = response.json()
 
