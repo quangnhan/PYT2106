fc62d61ff4f545e6575162ee93b4a19685102941
quangnhan145 <nhan@leadplus.net>
2021-08-17 20:10:32 +0700

    Modifited baitap day12

--- Day12/nhan/get.py
+++ Day12/nhan/get.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://611ba6e422020a00175a460d.mockapi.io/User"
+url = "https://611ba6e422020a00175a460d.mockapi.io/users"
 response = requests.get(url)
 data = response.json()
 
