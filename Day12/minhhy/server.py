4c20087cf49ba89ef7bbc29f70374922d5499dac
b-tuk <b.tintuk@gmail.com>
2021-08-17 21:03:14 +0700

    minhhy - bai12 - gmail send server class

--- Day12/minhhy/server.py
+++ Day12/minhhy/server.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import smtplib, ssl
 
 class Server:
     # private variable
@@ -40,6 +42,22 @@ class Server:
         r = requests.delete(f"{self.__url}/User/{id}")
         return(r.json())
 
+    def send_gmail(self, receiver_email):
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        sender_email = "b.tintuk@gmail.com"
+        receiver_email = receiver_email
+        password = os.environ['GMAIL_TOKEN']  #
+        message = f"""\
+        Subject: Hi there {receiver_email}
+
+        This message is sent from Python."""
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+
 if __name__ == "__main__":
     server = Server()
     data = server.get_user()
@@ -64,3 +82,6 @@ if __name__ == "__main__":
     # delete_user
     id = 7
     print(server.delete_user(id))
+
+    # send_gmail
+    server.send_gmail('b.tintuk@gmail.com')
