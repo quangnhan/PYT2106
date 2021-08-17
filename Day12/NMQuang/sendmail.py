013ddbcb70748766ffe55a43dee46b5e6044140d
RayNguyen842857 <ray_nguyen842857@ray-as.tech>
2021-08-17 23:37:48 +0900

    update

--- Day12/NMQuang/sendmail.py
+++ Day12/NMQuang/sendmail.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+from server import Server
 
 
 class Mail:
@@ -6,7 +7,6 @@ class Mail:
     __smtp_server = "smtp.gmail.com"
     __sender_email = "quangnhan145@gmail.com"  # Enter your address
     __password = "pgmwzphlbmkkrkrb"
-
 
     def send_mail(self, receiver_email):
         message = """\
@@ -19,3 +19,26 @@ class Mail:
         with smtplib.SMTP_SSL(self.__smtp_server, self.__port, context=context) as server:
             server.login(self.__sender_email, self.__password)
             server.sendmail(self.__sender_email, receiver_email, message)
+
+    def congratulate(self, receiver_email):
+        message = """\
+        Subject: Congratulation!
+
+        This message is sent from Python."""
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(self.__smtp_server, self.__port, context=context) as server:
+            server.login(self.__sender_email, self.__password)
+            server.sendmail(self.__sender_email, receiver_email, message)
+
+
+def main():
+    server = Server()
+    mail = Mail()
+    users = server.get_user()
+    for user in users:
+        mail.congratulate(user)
+
+
+if __name__ == "__main__":
+    main()
