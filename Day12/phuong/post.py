129f920685abde77c986a0cd356cc33424c55f3c
Calcierzz <phuongnv510@gmail.com>
2021-08-17 19:51:32 +0700

    updt

--- Day12/phuong/post.py
+++ Day12/phuong/post.py
@@ -1,14 +1,12 @@
 import requests
 
-id = ({
+data = ({
     'name': "phuong",
     'colour': "mongoloid",
     'id': 1
 })
-url = f"https://611ba8ad22020a00175a4622.mockapi.io/fruit_name/{id}"
-response = requests.get(url)
+url = f"https://611ba8ad22020a00175a4622.mockapi.io/fruit_name/"
+response = requests.get(url, data=data)
 data = response.json()
 
 print(data)
-
-
