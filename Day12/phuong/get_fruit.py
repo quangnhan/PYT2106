6704cf52a1290245cebc68bab3564eb53e21e878
Calcierzz <phuongnv510@gmail.com>
2021-08-17 19:48:49 +0700

    updt

--- Day12/phuong/get_fruit.py
+++ Day12/phuong/get_fruit.py
@@ -1,7 +1,6 @@
 import requests
 
-id = 1
-url = f"https://611ba8ad22020a00175a4622.mockapi.io/fruit_name/{id}"
+url = f"https://611ba8ad22020a00175a4622.mockapi.io/fruit_name"
 response = requests.get(url)
 data = response.json()
 
