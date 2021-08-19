9492729787037839c7a0d99ac457133d6e1d13d8
hythang2020 <hythang2020@gmail.com>
2021-08-19 20:14:39 +0700

    up

--- Day13/Hy/clients.py
+++ Day13/Hy/clients.py
@@ -1,8 +1,9 @@
 from Director import Director
 from BuilderTreeHouse import BuilderTreeHouse
+from BuilderCastle import BuilderCastle
 
-
-builder = BuilderTreeHouse()
+# builder = BuilderTreeHouse()
+builder = BuilderCastle()
 house = builder.get_result()
 director = Director(builder)
 
