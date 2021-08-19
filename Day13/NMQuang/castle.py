194a7b8d266a1826519619afafaaff08bb2d53da
RayNguyen842857 <ray_nguyen842857@ray-as.tech>
2021-08-19 23:29:03 +0900

    update

--- Day13/NMQuang/castle.py
+++ Day13/NMQuang/castle.py
@@ -2,6 +2,7 @@ class Castle:
     walls = 0
     windows = 0
     doors = 0
+    pools = 0
 
     def show(self):
-        print(f'This Castle has {self.walls} wall(s), {self.windows} window(s) and {self.doors} door(s).')
+        print(f'This Castle has {self.walls} wall(s), {self.windows} window(s), {self.doors} door(s) and {self.pools} pool(s).')
--- Day13/NMQuang/treehousebuilder.py
+++ Day13/NMQuang/treehousebuilder.py
@@ -1,18 +1,7 @@
+from builder import Builder
 from treehouse import TreeHouse
 
 
-class TreeHouseBuilder:
+class TreeHouseBuilder(Builder):
     def __init__(self):
-        self.__result = TreeHouse()
-    
-    def build_wall(self, number):
-        self.__result.walls = number
-
-    def build_door(self, number):
-        self.__result.doors = number
-
-    def build_window(self, number):
-        self.__result.windows = number
-
-    def get_result(self):
-        return self.__result
+        self._result = TreeHouse()
