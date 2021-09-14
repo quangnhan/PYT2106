83842d3cef245d107090f24ee9147633aaed5019
unknown <forexhte@gmail.com>
2021-09-14 20:07:19 +0700

    creat

---
+++ Django1/duc/blogs/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def blog_list(request):
+    if request.method == 'GET':
+        list_all_blog = [
+            {'id': 1, 'name': 'name1'},
+            {'id': 2, 'name': 'name2'},
+        ]
+        data = {
+            'list_all_blog': list_all_blog
+        }
+        return render(request, 'apps/blogs/blog_list.html',data)
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        return HttpResponse(f"Create blog {name} success")
+
+# Create your views here.
