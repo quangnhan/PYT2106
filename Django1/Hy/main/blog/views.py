c95501e84f8ca29c8f4a178f98eb57af602c7c14
hythang2020 <hythang2020@gmail.com>
2021-09-14 19:55:36 +0700

    up

--- Django1/Hy/main/blog/views.py
+++ Django1/Hy/main/blog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 def blog_list(request):
-    return render(request, 'apps/blogs/blog_list.html')
+    if request.method == 'GET':
+
+        list_all_blog = [
+            {'id' :1,'name' :'name1' },
+            {'id' :1,'name' :'name1' },
+            {'id' :1,'name' :'name1' },
+            {'id' :1,'name' :'name1' }
+        ]
+        data = {'blogs' : list_all_blog}
+        return render(request, 'apps/blogs/blog_list.html',data)
+    
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        return HttpResponse(f"Create blog {name} success")
