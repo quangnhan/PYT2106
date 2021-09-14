f74547793cc1ee157870026e1a3beb16b5ee8fcf
quangnhan145 <nhan@leadplus.net>
2021-09-14 21:30:13 +0700

    Create database

--- Django1/nhan/blogs/models.py
+++ Django1/nhan/blogs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, default="Empty")
+
+    def __str__(self):
+        return self.name
