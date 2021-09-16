ad2e01abd385bfa18c4b731eef7ff899e47c15fd
quangnhan145 <nhan@leadplus.net>
2021-09-16 19:08:52 +0700

    Split list and create view

--- Django1/nhan/blogs/urls.py
+++ Django1/nhan/blogs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import blog_create, BlogListView
+from .views import BlogCreateView, blog_create, BlogListView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name="blog_list"),
-    path('create', blog_create, name="blog_create"),
+    path('create', BlogCreateView.as_view(), name="blog_create"),
+    path('post_create', blog_create, name="blog_post_create"),
 ]
