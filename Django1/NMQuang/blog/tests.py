ed13a322fc4bba20fd6111daf0ea05124d7d8a13
RayNguyen842857 <Ray_Nguyen842857@ray-as.tech>
2021-09-16 23:04:50 +0900

    nothing special

--- Django1/NMQuang/blog/tests.py
+++ Django1/NMQuang/blog/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class BlogTest(TestCase):
+    def test_blog_list_view(self):
+        response = self.client.get(reverse('blog_list'))
+        self.assertEqual(response.status_code, 200)
