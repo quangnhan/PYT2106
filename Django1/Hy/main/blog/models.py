99be698a58576539af65b4767443ada16dc7a0bf
hythang2020 <hythang2020@gmail.com>
2021-09-19 21:40:22 +0700

    category by blog

--- Django1/Hy/main/blog/models.py
+++ Django1/Hy/main/blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Blog(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-
+    
     #Ten hien thi trong CSDL
     def __str__(self):
         return self.name
