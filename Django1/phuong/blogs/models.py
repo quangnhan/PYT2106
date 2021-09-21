8a53c76604a566169e5370a265753280e94a7af8
Calcierzz <phuongnv510@gmail.com>
2021-09-21 14:47:36 +0700

    updt

--- Django1/phuong/blogs/models.py
+++ Django1/phuong/blogs/models.py
@@ -4,4 +4,7 @@ from django.db import models
 
 class Blog(models.Model):
     name = models.CharField(max_length=100)
-    
+    description = models.CharField(max_length=100, default="Empty")
+
+    def __str__(self):
+        return self.name
