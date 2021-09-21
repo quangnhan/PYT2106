ceb7aa39ff5e8996b8331b94262d1033c8f4d066
hythang2020 <hythang2020@gmail.com>
2021-09-21 20:03:59 +0700

    up category by id

--- Django1/Hy/main/blog/models.py
+++ Django1/Hy/main/blog/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
 class Blog(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     #Ten hien thi trong CSDL
     def __str__(self):
         return self.name
