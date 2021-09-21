d7c8ca9be50db00ff1f1d915af1c13f5cbc671a7
unknown <forexhte@gmail.com>
2021-09-21 19:56:36 +0700

    update

--- Day12/duc/get.by_id.py
+++ Day12/duc/get.by_id.py
@@ -1,5 +1,5 @@
 import requests
-id = 5
+id = 3
 url = f"http://611d12f77d273a0017e2f612.mockapi.io/User{id}"
 response = requests.get(url)
 data = response.json()
---
+++ Day13/duc/House.py
@@ -0,0 +1,7 @@
+class House:
+    walls = 0
+    window = 0
+    doors = 0
+
+    def show(self):
+        print("House had {self.walls} wall,{self.windown} window, {self.doors} door")
