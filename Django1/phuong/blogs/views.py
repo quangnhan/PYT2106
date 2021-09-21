06062fea860a06fe633cc8258a81bd8b6d2a4ef2
Calcierzz <phuongnv510@gmail.com>
2021-09-21 20:38:21 +0700

    updt

--- Django1/phuong/blogs/views.py
+++ Django1/phuong/blogs/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from .models import Blog
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Blog, Category
 
 # Create your views here.
 
-class BlogListView(TemplateView):
+class CategoryListView(TemplateView):
+    template_name = "apps/blogs/category_list.html"
+    model = Category
+    context_object_name = 'list_all_category'
+
+class BlogListView(ListView):
     template_name = "apps/blogs/blog_list.html"
+    model = Blog
 
     def get_context_data(self, *args, **kwargs):
+        #get_data
+        category_id = self.request.GET.get('category_id', None)
         context = super().get_context_data(*args, **kwargs)
-        list_all_blog = Blog.objects.all
+
+        #Get list blog by category
+        list_all_blog = Blog.objects.filter(category__id = category_id)
         context['list_all_blog'] = list_all_blog
         return context
 
@@ -22,3 +34,7 @@ def blog_create(request):
         name = request.POST.get('name')
         Blog.objects.create(name=name)
         return HttpResponse(f"Create blog {name} success")
+
+class BlogDetailView(DetailView):
+    template_name = 'apps/blogs/blog_detail.html'
+    mode = Blog
