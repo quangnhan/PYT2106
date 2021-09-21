376339b8b02a7c9f72d2102e5f0873a24484bc16
Viet Phuong <vietphuong@Viets-MacBook-Pro.local>
2021-09-21 18:33:56 +0700

    updt

--- Django1/phuong/blogs/models.py
+++ Django1/phuong/blogs/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class Blog(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100, default="Empty")
 
-    def __str__(self):
-        return self.name
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    # def __str__(self):
+    #     return self.name
