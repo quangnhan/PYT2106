e6308fbfa95963285a7672ed207e70c4f2da5c06
quangnhan145 <nhan@leadplus.net>
2021-09-21 21:35:45 +0700

    Create delete update view

--- Django1/nhan/blogs/views.py
+++ Django1/nhan/blogs/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Blog, Category
 # Create your views here.
@@ -28,15 +28,6 @@ class BlogListView(ListView):
         context['list_all_blog'] = list_all_blog
         return context
 
-class BlogCreateV2View(TemplateView):
-    template_name = "apps/blogs/blog_create_2.html"
-
-def blog_create(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        Blog.objects.create(name=name)
-        return HttpResponse(f"Create blog {name} success")
-
 class BlogDetailView(DetailView):
     template_name = "apps/blogs/blog_detail.html"
     model = Blog
@@ -45,7 +36,29 @@ class BlogCreateView(CreateView):
     template_name = "apps/blogs/blog_create.html"
     model = Blog
     fields = ['name', 'description', 'category']
-    # success_url = reverse_lazy('category_list')
 
     def get_success_url(self, **kwargs):      
         return reverse_lazy('blog_detail', args = (self.object.id,))
+
+class BlogUpdateView(UpdateView):
+    template_name = "apps/blogs/blog_update.html"
+    model = Blog
+    fields = ['name', 'description', 'category']
+
+    def get_success_url(self, **kwargs):      
+        return reverse_lazy('blog_detail', args = (self.object.id,))
+
+class BlogDeteleView(DeleteView):
+    template_name = "apps/blogs/blog_delete.html"
+    model = Blog
+    success_url = reverse_lazy('category_list')
+
+# ==============================================================================================
+class BlogCreateV2View(TemplateView):
+    template_name = "apps/blogs/blog_create_2.html"
+
+def blog_create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        Blog.objects.create(name=name)
+        return HttpResponse(f"Create blog {name} success")
