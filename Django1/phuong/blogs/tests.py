11bcdffca1d8609d2b4d42dabed84ac1ee27771b
Viet Phuong <vietphuong@Viets-MacBook-Pro.local>
2021-09-21 18:10:03 +0700

    updt

--- Django1/phuong/blogs/tests.py
+++ Django1/phuong/blogs/tests.py
@@ -12,18 +12,19 @@ class BlogTest(TestCase):
         print("[Debugging...]"), reverse('blog_list')
         response = self.client.get(reverse('blog_List'))
         list_all_blog = response.context_data['list_all_blog']
-        
+
         self.assertEqual(list_all_blog.count(), 2)
         self.assertEqual(response.status_code, 200)
 
-    # def test_blog_create_view(self):
-    #     response = self.client.get(reverse('blog_create'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_blog_create_view(self):
+        response = self.client.get(reverse('blog_create'))
+        self.assertEqual(response.status_code, 200)
 
-    # def test_blog_create_post(self):
-    #     data = {'name': "blog"}
-    #     response = self.client.post(reverse('blog_post_create'), data)
-    #     self.assertEqual(response.status_code, 200)
-
-    #     list_all_blog = Blog.objects.all()
-    #     self.assertEqual(list_all_blog.count(), 3)
+    def test_blog_create_post(self):
+        data = {'name': "blog"}
+        response = self.client.post(reverse('blog_post_create'), data)
+        print(response)
+        self.assertEqual(response.status_code, 200)
+        list_all_blog = Blog.objects.all()
+        print(list_all_blog)
+        self.assertEqual(list_all_blog.count(), 3)
