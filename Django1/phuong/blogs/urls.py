8add8826e13dbb8032fb452ff65bac5cf25e19c9
Viet Phuong <vietphuong@Viets-MacBook-Pro.local>
2021-09-28 19:59:32 +0700

    re-do

--- Django1/phuong/blogs/urls.py
+++ Django1/phuong/blogs/urls.py
@@ -1,11 +1,24 @@
-from django.urls import path 
-from .views import blog_create, BlogListView, BlogCreateView, CategoryListView, BlogDetailView
+from django.urls import path
+from .views import (
+    BlogCreateView, 
+    blog_create, 
+    BlogListView, 
+    BlogDetailView, 
+    CategoryListView, 
+    BlogCreateV2View,
+    BlogUpdateView,
+    BlogDeteleView,
+)
 
 urlpatterns = [
-    path('category', CategoryListView.as_view, name="category_List"),
+    path('category', CategoryListView.as_view(), name="category_list"),
 
     path('', BlogListView.as_view(), name="blog_list"),
     path('create', BlogCreateView.as_view(), name="blog_create"),
-    path('post_create', blog_create, name="blog_post_create"),
     path('<int:pk>', BlogDetailView.as_view(), name="blog_detail"),
+    path('<int:pk>/update', BlogUpdateView.as_view(), name="blog_update"),
+    path('<int:pk>/delete', BlogDeteleView.as_view(), name="blog_delete"),
+
+    path('create/v2', BlogCreateV2View.as_view(), name="blog_create_v2"),
+    path('post_create', blog_create, name="blog_post_create"),
 ]
