98f0c60cc307e21038ef45028e0fe994e01e83bb
quangnhan145 <nhan@leadplus.net>
2021-10-05 19:52:14 +0700

    Customer model user

--- Django1/nhan/accounts/admin.py
+++ Django1/nhan/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+class CustomerUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+
+admin.site.register(CustomUser, CustomerUserAdmin)
--- Django1/nhan/blogs/models.py
+++ Django1/nhan/blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -11,7 +12,8 @@ class Category(models.Model):
 class Blog(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100, default="Empty")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, blank=True, null=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
