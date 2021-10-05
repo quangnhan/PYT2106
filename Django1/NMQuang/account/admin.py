4171d9ea6508e828f001af6b230ef47dd0ffe048
RayNguyen842857 <Ray_Nguyen842857@ray-as.tech>
2021-10-05 22:11:20 +0900

    nothing special

--- Django1/NMQuang/account/admin.py
+++ Django1/NMQuang/account/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from account.models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+class CustomerUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+
+
+admin.site.register(CustomUser, CustomerUserAdmin)
