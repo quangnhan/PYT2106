19536eeb2dc32daec3f2bd326b3a4a20bb551a0a
Viet Phuong <vietphuong@Viets-MacBook-Pro.local>
2021-10-05 19:54:02 +0700

    update

--- Django1/phuong/blogs/admin.py
+++ Django1/phuong/blogs/admin.py
@@ -3,5 +3,9 @@ from .models import Blog, Category
 
 # Register your models here.
 
+class BlogAdmin(admin.ModelAdmin):
+    model = Blog
+    list_display = ['name', 'category', 'description']
+
 admin.site.register(Blog) 
 admin.site.register(Category) 
