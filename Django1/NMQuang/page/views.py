461d68e0c6008a05b93784f4f72c02b6bacbc683
RayNguyen842857 <Ray_Nguyen842857@ray-as.tech>
2021-10-07 22:18:31 +0900

    nothing special

--- Django1/NMQuang/page/views.py
+++ Django1/NMQuang/page/views.py
@@ -1,8 +1,8 @@
 # from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
-class HomePageView(LoginRequiredMixin, TemplateView):
+class HomePageView(TemplateView):
     template_name = "apps/page/home.html"
