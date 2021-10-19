b7eb277fc03923ecf4f2d4671aba1a04d24c35aa
quangnhan145 <nhan@leadplus.net>
2021-10-19 21:30:58 +0700

    Router and viewset

--- Django Rest Framework/nhan/api/urls.py
+++ Django Rest Framework/nhan/api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path 
-from .views import ProductAPIView, ProductAPIDetail
+from django.urls import path
+from rest_framework import routers 
+from rest_framework.routers import SimpleRouter
+from .views import ProductAPIView, ProductAPIDetail, ProductAPIViewset
 
-urlpatterns = [
-    path('', ProductAPIView.as_view()),
-    path('<int:pk>', ProductAPIDetail.as_view()),
-]
+# urlpatterns = [
+#     path('', ProductAPIView.as_view()),
+#     path('<int:pk>', ProductAPIDetail.as_view()),
+# ]
+
+router = SimpleRouter()
+
+router.register('', ProductAPIViewset, basename='products')
+
+urlpatterns = router.urls
