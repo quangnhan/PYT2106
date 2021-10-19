21ac49e924edd37fb3dc741af277bf116e3547bd
quangnhan145 <nhan@leadplus.net>
2021-10-19 21:14:10 +0700

    Create CRUD api

--- Django Rest Framework/nhan/api/urls.py
+++ Django Rest Framework/nhan/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import ProductAPIView
+from .views import ProductAPIView, ProductAPIDetail
 
 urlpatterns = [
     path('', ProductAPIView.as_view()),
+    path('<int:pk>', ProductAPIDetail.as_view()),
 ]
--- Django Rest Framework/nhan/api/views.py
+++ Django Rest Framework/nhan/api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from products.models import Product
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView
 from .serializers import ProductSerializer
 # Create your views here.
 
-class ProductAPIView(ListAPIView):
+class ProductAPIView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductAPIDetail(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
