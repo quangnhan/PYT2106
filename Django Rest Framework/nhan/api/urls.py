8a95d34fa4348686caa4f37b18ca155dd6ebdce7
quangnhan145 <nhan@leadplus.net>
2021-10-26 21:09:40 +0700

    Create apiview

--- Django Rest Framework/nhan/api/urls.py
+++ Django Rest Framework/nhan/api/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from rest_framework import routers 
 from rest_framework.routers import SimpleRouter
-from .views import ProductAPIView, ProductAPIDetail, ProductAPIViewset, BlogAPIViewset
+from .views import ProductAPIView, ProductAPIDetail, ProductAPIViewset, BlogAPIViewset, TestApiView
 
-# urlpatterns = [
-#     path('', ProductAPIView.as_view()),
-#     path('<int:pk>', ProductAPIDetail.as_view()),
-# ]
+urlpatterns = [
+    # path('', ProductAPIView.as_view()),
+    # path('<int:pk>', ProductAPIDetail.as_view()),
+    path('test', TestApiView.as_view()),
+]
 
-router = SimpleRouter()
+# router = SimpleRouter()
 
-router.register('product', ProductAPIViewset, basename='products')
-router.register('blog', BlogAPIViewset, basename='blogs')
+# router.register('product', ProductAPIViewset, basename='products')
+# router.register('blog', BlogAPIViewset, basename='blogs')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
--- Django Rest Framework/nhan/api/views.py
+++ Django Rest Framework/nhan/api/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework import permissions
 from products.models import Product, Blog
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import ProductSerializer, BlogSerializer
 from .permissions import IsAuthorOrReadOnly
 # Create your views here.
@@ -25,3 +27,22 @@ class BlogAPIViewset(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+
+class TestApiView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    
+    def get(self, request):
+        data = {
+            "method" : "Get",
+            "name" : "Nathan",
+            "age" : "26",
+            "language" : "English",
+        }
+        return Response(data)
+
+    def post(self, request):
+        print(request.data)
+        data = {
+            "method" : "POST"
+        }
+        return Response(data)
